a70f11a4fe04b4feab1d19920114c8b1e443286b
Guido Sojnoczki <guidosoynozki@gmail.com>
2022-12-29 22:09:16 -0300

    header style

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,7 @@
+"use client";
+//no pude encontrar modo de hacerlo dinámico sin hacerlo del cliente
+
+import { useState } from 'react';
 import styles from './header.module.css'
 import Link from 'next/link'
 import {Be_Vietnam_Pro} from '@next/font/google';
@@ -7,9 +11,24 @@ import MenuButton from './MenuButton';
 
 const vietnamPro = Be_Vietnam_Pro({weight:'400'});
 
+
 export default function Header(){
+    const [scroll, setScroll] = useState(false);
+
+    const changeBackground = ()=>{
+        if (window.scrollY > 0){
+            setScroll(true);
+        }
+        else {
+            setScroll(false);
+        }
+    }
+    
+    window.addEventListener('scroll',changeBackground);
+    
+
     return(
-        <div className={styles.header}>
+        <div className={`${styles.header} ${scroll ? `${styles.scrollDown}` : ''}`}>
             <Link href='/'>
                 <Image src={Icon} alt='Flagon Icon' height={40} className={styles.headerIcon}/>
             </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,22 @@ const vietnamPro = Be_Vietnam_Pro({weight:'400'});
 
 export default function Home(){
     return(
-        <main className={styles.main}>
-            <h1 className={styles.mainTitle}>
-                <Image src={Logo} alt='Flagon Logo' height={170} className={styles.mainLogo}/>
-            </h1>
-            <h2 className={`${vietnamPro.className} ${styles.mainTitle}`}>Calidad y técnica en materiales únicos</h2>
-            <div className={styles.arrowContainer}>
-                <Image src={ArrowDown} alt=''/>
-            </div>
-        </main>
+        <div>
+            <main className={styles.main}>
+                <h1 className={styles.mainTitle}>
+                    <Image src={Logo} alt='Flagon Logo' height={170} className={styles.mainLogo}/>
+                </h1>
+                <h2 className={`${vietnamPro.className} ${styles.mainTitle}`}>Calidad y técnica en materiales únicos</h2>
+                <div className={styles.arrowContainer}>
+                    <Image src={ArrowDown} alt=''/>
+                </div>
+            </main>
+            <section>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eaque, quasi architecto quae reprehenderit incidunt, voluptates, sequi at deleniti veritatis omnis aspernatur impedit ab soluta? Perferendis vel enim minus magni.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eaque, quasi architecto quae reprehenderit incidunt, voluptates, sequi at deleniti veritatis omnis aspernatur impedit ab soluta? Perferendis vel enim minus magni.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eaque, quasi architecto quae reprehenderit incidunt, voluptates, sequi at deleniti veritatis omnis aspernatur impedit ab soluta? Perferendis vel enim minus magni.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos eaque, quasi architecto quae reprehenderit incidunt, voluptates, sequi at deleniti veritatis omnis aspernatur impedit ab soluta? Perferendis vel enim minus magni.</p>
+            </section>
+        </div>
     )
 }
